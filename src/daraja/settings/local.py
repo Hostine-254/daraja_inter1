ba194d4306eb5e90603aeb6c720d5848301bf363
Hostine-254 <hostineomondi2020@gmail.com>
2023-07-13 11:57:18 -0700

    edited local configs

--- src/daraja/settings/local.py
+++ src/daraja/settings/local.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'daraja.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "daraja_internet",
+        "USER": "postgres",
+        "PASSWORD": "ndingo9052",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
