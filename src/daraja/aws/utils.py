2dadd92a5355e689c7e4ed241999bf22b6fd40b1
Hostine-254 <hostineomondi2020@gmail.com>
2023-07-14 01:18:41 -0700

    edited production settings

--- src/daraja/aws/utils.py
+++
@@ -1,5 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage,S3StaticStorage
-
-#StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
-StaticRootS3BotoStorage = lambda: S3StaticStorage(location='static')
-MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
