7b3aa418007344c745f8969ef07682a6b10cb53b
Hostine-254 <hostineomondi2020@gmail.com>
2023-07-14 04:34:04 -0700

    permission class

--- src/mpesa/api/views.py
+++ src/mpesa/api/views.py
@@ -1,5 +1,6 @@
 
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 from mpesa.models import LNMOnline
 from mpesa.api.serializers import LNMOnlineSerializer
@@ -7,7 +8,7 @@ from mpesa.api.serializers import LNMOnlineSerializer
 class LNMCallbackUrlAPIView(CreateAPIView):
     queryset = LNMOnline.objects.all()
     serializer_class = LNMOnlineSerializer
-    #permission_classes = [IsAdminUser]
+    permission_classes = [AllowAny]
 
     def create(self, request,):
         print(request.data, "this is the request.data")
