d884a70fcc5c88fb5507f9dfb98efa7ba4888b6b
Hostine-254 <hostineomondi2020@gmail.com>
2023-07-13 23:59:59 -0700

    edited production settings

--- src/daraja/settings/production.py
+++ src/daraja/settings/production.py
@@ -129,8 +129,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = "/static/"
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
-STATICFILES_DIRS = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+#STATICFILES_DIRS = BASE_DIR / "staticfiles"
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-cdn-local")
 
 
