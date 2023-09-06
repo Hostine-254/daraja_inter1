793a2d8da83674c90a0c9e28f50990ba13420160
Hostine-254 <hostineomondi2020@gmail.com>
2023-09-06 09:02:38 +0300

    added database for other locale

--- src/samples/voucher_retrival.py
+++ src/samples/voucher_retrival.py
@@ -37,6 +37,42 @@ def get_Voucher(amount,locale):
                #print("Shs 300 Vouchers are almost depleted")
            else:
                return (ret,voucher_available)
+           
+    #code for Nairobi
+
+    if amount == 2 and locale=="Nairobi":
+        with shelve.open('dbms/db_Nb_Day') as db:
+           #db.update(data)
+           msg = "Shs 20 Vouchers are almost depleted"
+           ret = voucher_logic(db)
+           
+           #print(dict(db))
+           if ret == 0:
+              return (ret,msg)
+              #print("Shs 20 Vouchers are almost depleted")
+           else:
+              return (ret,voucher_available)
+    elif amount == 100 and locale=="Nairobi":
+        with shelve.open('dbms/db_Nb_Week') as db:
+           #db.update(data)
+           msg = "Shs 100 Vouchers are almost depleted"
+           ret = voucher_logic(db)
+           if ret == 0:
+              return (ret,msg)
+               #print("Shs 100 Vouchers are almost depleted")
+           else:
+              return (ret,voucher_available)
+    elif amount ==320 and locale=="Nairobi":
+        with shelve.open('dbms/db_Nb_Month') as db:
+           #db.update(data)
+           msg = "Shs 320 Vouchers are almost depleted"
+           ret = voucher_logic(db)
+           if ret == 0:
+               return (ret,msg)
+               #print("Shs 300 Vouchers are almost depleted")
+           else:
+               return (ret,voucher_available)
+
     else:
        pass
 
